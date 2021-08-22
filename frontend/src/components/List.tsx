b347f99a059a0f89891a23e9c16056b80a8cfc7e
avrcoelho <andrevrcoelho@hotmail.com>
2021-08-22 13:27:16 -0300

    feat: done

--- frontend/src/components/List.tsx
+++ frontend/src/components/List.tsx
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useEffect, useState, useRef, FormEvent } from "react";
+
+interface Item {
+  _id: string;
+  item: string;
+}
+
+const API_URL = String(import.meta.env.SNOWPACK_PUBLIC_API_URL);
 
 export const List = (): JSX.Element => {
+  const [items, setItems] = useState<Item[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    fetch(API_URL).then((response) =>
+      response.json().then((response) => {
+        setItems(response);
+      })
+    );
+  }, []);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    fetch(API_URL, {
+      method: "POST",
+      body: JSON.stringify({ item: inputRef.current?.value }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then((response) =>
+      response.json().then((response) => {
+        setItems((prevState) => [...prevState, response]);
+        inputRef.current!.value = "";
+      })
+    );
+  };
+
   return (
     <main>
-      <form action="">
-        <input type="text" placeholder="Item" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={inputRef} placeholder="Item" />
 
         <button type="submit">Submit</button>
       </form>
+
+      <ul>
+        {items.map(({ item, _id }) => (
+          <li key={_id}>{item}</li>
+        ))}
+      </ul>
     </main>
   );
 };
